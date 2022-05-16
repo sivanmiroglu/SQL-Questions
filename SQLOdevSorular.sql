889a1d96e6b226a7a404ba9a6db7b4d7d6329dc4
sivanmiroglu <sivanmiroglu854@gmail.com>
2022-05-16 17:38:50 +0300

    SQL Questions

---
+++ SQLOdevSorular.sql
@@ -0,0 +1,174 @@
+﻿
+-- ÖDEV SORULAR * 
+
+--1) ürünleri ada göre sırala
+
+select * from Products order by ProductName
+
+--2) ürünleri ada göre tersten sırala
+
+select * from Products order by ProductName desc
+
+--3) ürün fiyatı 20 den büyük ve categoryid si 3 olan ürünleri fiyata göre sırala
+
+select * from Products where UnitPrice > 20 and CategoryID = 3
+
+--4) en pahalı 5 ürünü getir
+
+select top 5 * from Products order by UnitPrice desc
+
+--5) En pahalı ürünümün fiyatı
+
+select top 1 * from Products order by UnitPrice desc
+
+--6) En ucuz ürünümün fiyatı
+
+select top 1 * from Products order by UnitPrice 
+
+--7) En ucuz ürünümün KDV li fiyatı nedir?
+
+select top 1 UnitPrice*1.18 from Products order by UnitPrice
+
+--8) 1996 yılındaki siparişleri getir
+
+select * from Orders where YEAR(OrderDate) = 1996
+
+--9) 1997 yılının Mart ayının siparişlerini getir
+
+select * from Orders where Year(OrderDate) = 1997 and MONTH(OrderDate) = 3
+
+--10) ShipCity - 1997 yılında Londra'ya kaç adet sipariş gitti?
+
+select * from Orders where YEAR(OrderDate) = 1997 and ShipCity='London'
+
+--11) ProductID si 5 olan ürünün kategori adı nedir
+
+select  c.CategoryName from Products as p
+inner join Categories as c
+on p.ProductID = c.ID
+where ProductID =5
+
+
+--12) Ürün adı ve ürünün kategorisinin adı
+
+select p.ProductName, c.CategoryName from Products as p 
+inner join Categories as c 
+on p.ProductID = c.ID
+
+--13) Ürünün adı, kategorisinin adı ve tedarikçisinin adı
+
+select p.ProductName ,c.CategoryName, s.ContactName  from Products as p 
+inner join Categories as c on p.CategoryID= c.ID 
+inner join Suppliers as s on s.SupplierID = p.SupplierID
+ 
+--14) Siparişi alan personelin adı,soyadı, sipariş tarihi. Sıralama sipariş tarihine göre
+
+select FirstName, LastName, o.OrderDate  from Employees as e 
+inner join Orders o on e.EmployeeID = o.EmployeeID  order by OrderDate
+
+--15) Son 5 siparişimin ortalama fiyatı nedir? (sepet toplamı ortalaması)
+
+ select TOP 5  AVG(od.Quantity * od.UnitPrice) Toplam   from [Order Details] as od 
+ inner join Orders o on  od.OrderID = o.OrderID   group by od.OrderID  order by od.OrderID desc
+
+--16) Ocak ayında satılan ürünlerimin adı ve kategorisinin adı ve toplam satış miktarı nedir?
+
+select p.ProductName, c.CategoryName, od.Quantity from Products as p 
+inner join Categories as c on c.ID = p.CategoryID 
+inner join [Order Details] as od on od.ProductID = p.ProductID
+inner join Orders as ord on ord.OrderID = od.OrderID  where MONTH(OrderDate) = 01
+
+select SUM( od.Quantity) from Products as p 
+inner join Categories as c on c.ID = p.CategoryID 
+inner join [Order Details] as od on od.ProductID = p.ProductID
+inner join Orders as ord on ord.OrderID = od.OrderID  where MONTH(OrderDate) = 01 -- ürünlerin adı ve toplam satış miktarı listele.
+
+--17) Hangi personelim hangi personelime rapor veriyor?
+
+select es.EmployeeID , e.LastName , e.FirstName from Employees e
+inner join Employees es on e.EmployeeID = es.ReportsTo
+
+--18) Hangi ülkeden kaç müşterimiz var (distinc ve count kullanılacak)
+
+ select DISTINCT country, COUNT(*) as Count from Customers group by Country
+
+--19) Ortalama satış miktarımın üzerindeki satışlarım nelerdir? (order details tablosu)
+
+select * from [Order Details] where  Quantity > (select  avg(Quantity) from  [Order Details])
+
+--20) En çok satılan ürünümün(adet bazında) adı, kategorisinin adı ve tedarikçisinin adı (4 tablo birleşimi)
+
+select top 1 ProductName, CategoryName, ContactName from Products p 
+inner join [Order Details] od on od.ProductID = p.ProductID 
+inner join Categories c on c.ID = p.CategoryID 
+inner join Suppliers s on s.SupplierID = p.SupplierID order by od.Quantity desc
+
+--21) 10248 numaralı siparişi alan çalışanın adı ve soyadı ve orderid
+
+select e.FirstName, e.LastName ,o.OrderID from Employees e 
+inner join orders o on o.EmployeeID = e.EmployeeID
+where o.OrderID = 10248
+
+--22) 1996 yılında, 5 numaralı ID ye sahip çalışanım kaç adet sipariş aldı?
+	
+select SUM(Quantity) SiparisAdedi from [Order Details] od 
+inner join orders o on o.OrderID = od.OrderID
+inner join Employees e on e.EmployeeID = o.EmployeeID
+inner join Products p on p.ProductID = od.ProductID
+where YEAR(o.OrderDate) = 1996 and e.EmployeeID =5
+
+--23) 1997 yılında kim ne kadar sipariş geçti (EmployeeID, Count)
+
+select e.EmployeeID ,SUM(Quantity) SiparisAdedi from [Order Details] od 
+inner join orders o on o.OrderID = od.OrderID
+inner join Employees e on e.EmployeeID = o.EmployeeID
+inner join Products p on p.ProductID = od.ProductID
+where YEAR(o.OrderDate) = 1997 group by e.EmployeeID
+
+--24) 10248 numaralı siparişin ürünlerinin adları ve sipariş miktarı
+
+select p.ProductName , od.Quantity from Products p 
+inner join [Order Details] od on od.ProductID = p.ProductID
+where od.OrderID = 10248
+
+--25) 10248 numaralı siparişin toplam fiyatı
+
+select SUM(UnitPrice * Quantity) from Orders o 
+inner join  [Order Details] od
+on  o.OrderID = od.OrderID where o.OrderID = 10248
+
+--26) 1996 yılında cirom
+
+select SUM(UnitPrice * Quantity) from [Order Details] od inner join
+Orders o
+on o.OrderID = od.OrderID where YEAR(o.OrderDate) = 1996
+
+--27) 1996 yılında en çok ciro yapan employeeID
+
+select top 1 e.EmployeeID ,SUM(od.UnitPrice * od.Quantity) Ciro from [Order Details] od
+inner join Orders o
+on o.OrderID = od.OrderID
+inner join Employees e
+on e.EmployeeID = o.EmployeeID
+where YEAR(o.OrderDate) = 1996 group by e.EmployeeID  order by ciro desc
+
+--28) 1997 Mart ayındaki siparişlerimin ortalama fiyatı nedir?
+
+select AVG(od.UnitPrice * od.Quantity) from [Order Details] od 
+inner join orders o on o.OrderID = od.OrderID
+inner join Products p on p.ProductID = od.ProductID
+where YEAR(o.OrderDate) = 1997 and MONTH(o.OrderDate) = 03
+
+--29) 1997 yılındaki aylık satışları sırala. Ocak - 500 gibi toplamda 12 satır olacak
+
+select MONTH(o.OrderDate) Ay, sum(od.UnitPrice * od.Quantity) Satis from [Order Details] od 
+inner join Orders o on o.OrderID = od.OrderID 
+where YEAR(o.OrderDate) = 1997 Group by MONTH(o.OrderDate) order by (MONTH(o.OrderDate))
+
+
+--30) En değerli MÜŞTERİMİN adı ve soyadı (orders,orderdetails,customers)
+
+select c.ContactName , sum(od.UnitPrice * od.Quantity) Tutar  from Customers c 
+inner join Orders o on o.CustomerID = c.CustomerID 
+inner join [Order Details] od on od.OrderID = o.OrderID 
+group by c.ContactName order by Tutar desc
---
+++ SQLQuery1.sql
@@ -0,0 +1,128 @@
+﻿--Öğrenci => Ad, Soyad, No, EMail, Telefon, DogumTarih
+--Eğitmen => Ad, Soyad
+--Bölüm => Ad, Kurulus Tarih
+-- Fakulte => Ad
+
+--Not: Bir bölüm sadece bir fakülteye aittir.
+-- Bir eğitmen aynı anda iki bölüme ait olabilir.
+-- Bir öğrencinin sadece bir bölümü vardır.
+
+--Sorgular
+-- Öğrenci Ad, Soyad, Bölüm Ad, Fakülte Ad
+-- Eğitmen Ad, Bölüm Ad
+-- Fen Fakültesindeki EĞİTMENLERİ LİSTELE
+-- Fakülte ekleyen bir store procedure yaz
+-- Herhangi bir fakülte eklendiğinde ekrana 'Yeni bir fakülte eklendi!' yazsın (TRIGGER)
+-- Bölüm ID, Ad, Fakülte Ad isimli 3 kolonu olan bir VIEW yaz
+
+Create table  Student (
+ID int PRIMARY KEY IDENTITY(1,1),
+FirstName nvarchar(25),
+LastName nvarchar(25),
+StundentNumber nvarchar(10),
+EMail nvarchar (20),
+Number nvarchar (11),
+BirthDay date,
+DeparmentID  int NOT NULL
+)
+
+CREATE TABLE Academician (
+ID int PRIMARY KEY IDENTITY(1,1),
+FirstName nvarchar(25),
+LastName nvarchar(25)
+)
+
+CREATE TABLE Department (
+ID int PRIMARY KEY IDENTITY(1,1),
+DepartmentName nvarchar (30),
+PublishDate date,
+FacultyID int NOT NULL
+)
+
+CREATE TABLE Faculty
+(
+ID int PRIMARY KEY IDENTITY(1,1),
+FacultyName nvarchar (25),
+)
+
+create TABLE AcademicianDepartment (
+ID int PRIMARY KEY IDENTITY(1,1),
+AcademicianID int,
+DepartmentID int
+
+)
+
+insert into AcademicianDepartment(AcademicianID,DepartmentID) values(1,2)
+
+insert into Student(FirstName,LastName,StundentNumber,DeparmentID) values ('Burak','Kızılhan',1232,1)
+
+insert into Student(FirstName,LastName,StundentNumber,DeparmentID) values ('Ahmet','Cayar',1432,2)
+
+insert into Student(FirstName,LastName,StundentNumber,DeparmentID) values ('Mahmut','Aynacı',1764,3)
+
+insert into Academician(FirstName,LastName) values('İlber','Ortaylı')
+
+insert into Academician(FirstName,LastName) values('Cem','Kurt')
+
+insert into Academician(FirstName,LastName) values('Can','Özcan')
+
+insert into Department(DepartmentName,PublishDate,FacultyID) values('Park ve Bahçeler Müdürlüğü','1992',1)
+
+insert into Department(DepartmentName,PublishDate,FacultyID) values('Rehberlik','1999',2)
+
+insert into Department(DepartmentName,PublishDate,FacultyID) values('Fen','2021',3)
+
+insert into Department(DepartmentName,PublishDate,FacultyID) values('Fizik','2021',7)
+
+insert into Faculty(FacultyName) values('Çevrecilik')
+
+insert into Faculty(FacultyName) values('Fen')
+
+insert into Faculty(FacultyName) values('Okuma')
+
+insert into AcademicianDepartment(AcademicianID,DepartmentID) values(3,4)
+
+
+-- Öğrenci Ad, Soyad, Bölüm Ad, Fakülte Ad
+select s.FirstName,s.LastName,d.DepartmentName,f.FacultyName from Student s 
+inner join Department d on s.DeparmentID = d.ID
+inner join Faculty f on f.ID  = d.FacultyID 
+
+-- Eğitmen Ad, Bölüm Ad
+select a.FirstName ,d.DepartmentName  from Academician a 
+inner join AcademicianDepartment ad on ad.AcademicianID = a.ID
+inner join Department d on d.ID = ad.DepartmentID
+
+-- Fen Fakültesindeki EĞİTMENLERİ LİSTELE
+select f.FacultyName,a.ID, a.FirstName,a.LastName from AcademicianDepartment ad
+inner join Academician a on a.ID = ad.AcademicianID 
+inner join Department d on d.ID = ad.DepartmentID
+inner join Faculty f on f.ID = d.FacultyID where f.FacultyName like '%Fen%'
+
+-- Fakülte ekleyen bir store procedure yaz
+CREATE PROCEDURE sp_AddFaculty(@Name nvarchar(25))
+as
+begin 
+insert into Faculty (FacultyName) values (@Name)
+end
+
+sp_AddFaculty 'Hayvanları Koruma'
+
+-- Herhangi bir fakülte eklendiğinde ekrana 'Yeni bir fakülte eklendi!' yazsın (TRIGGER)
+
+create trigger trg_add__faculty
+on Faculty 
+after insert
+as
+begin
+print ('Yeni bir fakülte eklendi!')
+end
+
+-- Bölüm ID, Ad, Fakülte Ad isimli 3 kolonu olan bir VIEW yaz
+
+create view vw_DepartmentandFaculty1
+as
+select d.ID,d.DepartmentName,f.FacultyName from Department d 
+inner join Faculty f on f.ID = d.FacultyID 
+
+select * from vw_DepartmentandFaculty
